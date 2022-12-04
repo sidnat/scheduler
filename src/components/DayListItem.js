c8441ad7a8639c2f03e76c11e38b29207a7ed4fb
Sid Natarajan <sid.nat@gmail.com>
2022-12-03 23:23:08 -0500

    Testing the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,24 @@ export default function DayListItem(props) {
     "button--danger": props.spots === 0
   });
 
+  const formatSpots = (spots) => {
+    let formattedSpot = `${spots} spots remaining`;
+
+    if (spots === 0) {
+      return 'no spots remaining';
+    }
+
+    if (spots === 1) {
+      return '1 spot remaining';
+    }
+
+    return formattedSpot;
+  };
+
   return (
     <li className={buttonClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
