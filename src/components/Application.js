a66c8240315ea5936085913bee978070adc34296
Sid Natarajan <sid.nat@gmail.com>
2022-12-05 15:37:09 -0500

    Managing State

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,14 +45,17 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState(["Monday"]);
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     const testURL = `http://localhost:8001/api/days`;
     axios.get(testURL).then(response => {
-      console.log(response.data);
-      setDays([...response.data]);
+      setDays(response.data);
     });
   }, []);
 
@@ -76,8 +79,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
