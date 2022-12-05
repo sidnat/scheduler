fda098e1c981d86f2cfb3a7e1587cb1c983120be
Sid Natarajan <sid.nat@gmail.com>
2022-12-04 23:03:00 -0500

    Props And State in the Appointment Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,33 +1,56 @@
-import React from "react";
-import Button from "components/Button"
+import React, { useState } from 'react';
+import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  }
+
+  const cancel = () => {
+    reset()
+    props.onCancel()
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={submitForm}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            onChange={(event) => setStudent(event.target.value)}
+            value={student}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
   );
 }
+
+// student:String
+// interviewer:Number
+// interviewers:Array
+// onSave:Function
+// onCancel:Function
--- stories/index.js
+++ stories/index.js
@@ -171,6 +171,13 @@ storiesOf("Appointment", module)
       onClose={action("onClose")}
     />
   ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
   .add("Edit", () => (
     <Form
       student="Sid"
@@ -180,10 +187,3 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
-  .add("Create", () => (
-    <Form
-      interviewers={interviewers}
-      onSave={action("onSave")}
-      onCancel={action("onCancel")}
-    />
-  ));
